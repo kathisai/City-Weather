7ab5c53422dd1d421206b2dad6a15ea12b399e1d
Kathi sai prathap reddy <sai.990880@gmail.com>
2021-11-21 23:14:39 +0530

    Update Constants.kt

--- app/src/main/java/com/prathap/weather/utils/Constants.kt
+++ app/src/main/java/com/prathap/weather/utils/Constants.kt
@@ -2,7 +2,6 @@ package com.prathap.weather.utils
 
 object Constants {
     // add to git secret
-//    const val API_KEY = "691733c12c81dde81a5bb9d238d033b0"
-    const val API_KEY = "fae7190d7e6433ec3a45285ffcf55c86"
+    const val API_KEY = "fae7190d7e6433ecdsfasdfa85ffcf55c86"
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 }
